0eaac79567114ad7598edf8464e29bc789b7322c
Panagiotis3149 <138581609+Panagiotis3149@users.noreply.github.com>
2024-08-07 19:05:04 +0000

    Update QNG.c

--- QNG.c
+++ QNG.c
@@ -6,20 +6,26 @@
 #include <omp.h>
 #include <windows.h>
 
+#define WEBHOOK_URL "YOUR WEBHOOK URL" // change this
 
-#define WEBHOOK_URL "YOUR WEBHOOK URL"
-
+uint32_t xorshift_state = 1;
+uint32_t xorshift() {
+    xorshift_state ^= (xorshift_state << 13);
+    xorshift_state ^= (xorshift_state >> 17);
+    xorshift_state ^= (xorshift_state << 5);
+    return xorshift_state;
+}
 
 char* generate_string() {
     static char str[19];
     for (int i = 0; i < 18; i++) {
-        if (rand() % 2) {
-            str[i] = (rand() % 26) + 'a'; 
+        int type = xorshift() % 3;
+        if (type == 0) {
+            str[i] = (xorshift() % 26) + 'a'; 
+        } else if (type == 1) {
+            str[i] = (xorshift() % 26) + 'A'; 
         } else {
-            str[i] = (rand() % 26) + 'A'; 
-        }
-        if (rand() % 3 == 0) {
-            str[i] = (rand() % 10) + '0'; 
+            str[i] = (xorshift() % 10) + '0';
         }
     }
     str[18] = '\0';
@@ -38,12 +44,11 @@ int send_request(CURL* curl, const char* code) {
         long response_code;
         curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
         if (response_code == 200) {
-            return 1; 
+            return 1;
         }
     }
     return 0;
 }
-
 
 void send_webhook(CURL* curl, const char* code) {
     CURLcode res;
@@ -59,30 +64,26 @@ void send_webhook(CURL* curl, const char* code) {
 }
 
 int main() {
-    srand(time(NULL)); 
+    srand(time(NULL));
     int num_cores = omp_get_max_threads();
+    omp_set_num_threads(num_cores * 16); // change this 
+
     printf("CPU CORES: %d\n", num_cores);
-    printf("THREADS: %d x\n", num_cores);
+    printf("THREADS: %d x\n", num_cores * 16); // change this
 
     curl_global_init(CURL_GLOBAL_DEFAULT);
 
-    #pragma omp parallel num_threads(num_cores * 4)
+    #pragma omp parallel
     {
         CURL* curl;
         curl = curl_easy_init();
 
         while (1) {
             char* code = generate_string();
-            printf("Attempting to redeem code %s...\n", code);
 
             if (send_request(curl, code)) {
-                printf("Code %s is valid! Sending to webhook...\n", code);
                 send_webhook(curl, code);
-            } else {
-                printf("Code %s is invalid.\n", code);
             }
-
-            
         }
 
         curl_easy_cleanup(curl);
@@ -92,4 +93,3 @@ int main() {
 
     return 0;
 }
-
